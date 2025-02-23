da708982aba09c9b73e7a24f9ce88359a4163545
Abdeljabbar Elkharroubi <origami.design12@gmail.com>
2025-02-23 22:19:39 +0000

    Add MyPage component with CSS styles

---
+++ alx-react-app/MyPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MyPage.css';
+
+const MyPage = () => {
+    return (
+        <div className="my-page">
+            <h1>Welcome to My Page</h1>
+            <p>This is a simple example of a React page.</p>
+        </div>
+    );
+};
+
+export default MyPage;
