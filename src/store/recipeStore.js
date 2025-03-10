b20bfeeefa6047e76cefa8775a545880cbdedccc
Abdeljabbar Elkharroubi <origami.design12@gmail.com>
2025-03-10 03:29:04 +0000

    Add detailed recipe management: view, edit, and delete recipes

--- src/store/recipeStore.js
+++ src/store/recipeStore.js
@@ -1,16 +1,16 @@
-// src/store/recipeStore.js
-
 import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (id) => set((state) => ({ recipes: state.recipes.filter(recipe => recipe.id !== id) })),
-  updateRecipe: (updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map(recipe => 
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    )
-  }))
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    ),
+  })),
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
 }));
 
 export { useRecipeStore };
