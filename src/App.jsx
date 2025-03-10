7de4b8ccd2c240515ddedae6feadfc371cdd24dd
Abdeljabbar Elkharroubi <origami.design12@gmail.com>
2025-03-10 03:12:01 +0000

    Removed embedded Git repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useRecipeStore } from "./components/recipeStore";  // Correct the path
-import RecipeDetails from './components/RecipeDetails'; // Ensure the correct path
-import RecipeList from './components/RecipeList'; // Add path for RecipeList component
+import { useRecipeStore } from "./store/recipeStore";  // Correct path
+import RecipeDetails from './components/RecipeDetails';  // Ensure the correct path
+import RecipeList from './components/RecipeList';  // Ensure the correct path
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RecipeList />} />  {/* Recipe list page */}
-        <Route path="/recipes/:id" element={<RecipeDetails />} />  {/* Recipe details page */}
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} /> {/* Recipe details page */}
       </Routes>
     </Router>
   );
--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from "../component/recipeStore";
+import { useRecipeStore } from "../store/recipeStore";
 import { Link } from 'react-router-dom';  // Import Link from react-router-dom to navigate
 
 const RecipeList = () => {
