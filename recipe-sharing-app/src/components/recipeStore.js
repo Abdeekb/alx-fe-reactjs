0807217f61b7c1ea02bc9a9f21acb2e789ded74c
Abdeljabbar Elkharroubi <origami.design12@gmail.com>
2025-03-10 01:24:17 +0000

    Add setRecipes function to recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,7 @@ import { create } from 'zustand';
 const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+  setRecipes: (recipes) => set({ recipes }), // إضافة دالة setRecipes
 }));
 
 export default useRecipeStore;
