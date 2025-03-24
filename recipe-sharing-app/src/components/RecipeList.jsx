f8a63b39ce3313559e91958b332f2782dacbf6e4
Abdeljabbar Elkharroubi <origami.design12@gmail.com>
2025-03-24 00:53:58 +0000

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,35 +1,48 @@
+// src/components/RecipeList.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useRecipeStore from '../store/recipeStore';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
-  
+  // Access recipes from the store
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  // If there are no recipes, show a message
   if (recipes.length === 0) {
     return (
-      <div>
+      <div className="recipe-list">
         <h2>Recipes</h2>
         <p>No recipes yet. Add your first recipe!</p>
-        <Link to="/add">Add Recipe</Link>
+        <Link to="/add" className="add-recipe-link">Add Your First Recipe</Link>
       </div>
     );
   }
-  
+
+  // Render the list of recipes
   return (
-    <div>
+    <div className="recipe-list">
       <h2>Recipes</h2>
-      <ul>
-        {recipes.map(recipe => (
-          <li key={recipe.id}>
+      <div className="recipes-container">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="recipe-card">
             <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-            <Link to={`/recipes/${recipe.id}`}>View Details</Link> | 
-            <Link to={`/recipes/${recipe.id}/edit`}>Edit</Link> | 
-            <DeleteRecipeButton recipeId={recipe.id} />
-          </li>
+            <p className="recipe-description">
+              {recipe.description.substring(0, 100)}
+              {recipe.description.length > 100 ? '...' : ''}
+            </p>
+            <div className="recipe-actions">
+              <Link to={`/recipes/${recipe.id}`} className="view-recipe-btn">
+                View Details
+              </Link>
+              <Link to={`/recipes/${recipe.id}/edit`} className="edit-recipe-btn">
+                Edit
+              </Link>
+              <DeleteRecipeButton recipeId={recipe.id} />
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
