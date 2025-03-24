a3d5c2c24614c37e7540d91063e65b779dc46734
Abdeljabbar Elkharroubi <origami.design12@gmail.com>
2025-03-24 00:00:10 +0000

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,34 @@
+// src/store/recipeStore.js
 import { create } from 'zustand';
 
+// Create a store to manage recipes
 const useRecipeStore = create((set) => ({
+  // Initial state
   recipes: [],
-  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
-  setRecipes: (recipes) => set({ recipes }), // إضافة دالة setRecipes
+  
+  // Action to add a new recipe
+  addRecipe: (newRecipe) => {
+    const recipeWithId = { ...newRecipe, id: Date.now() };
+    set((state) => ({ 
+      recipes: [...state.recipes, recipeWithId] 
+    }));
+    return recipeWithId;
+  },
+  
+  // Action to delete a recipe by id
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  })),
+  
+  // Action to update an existing recipe
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map(recipe => 
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    )
+  })),
+  
+  // Action to set the entire recipes array
+  setRecipes: (recipes) => set({ recipes })
 }));
 
 export default useRecipeStore;
